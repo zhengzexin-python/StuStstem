b0b4c4d0677a0a312754b845dc2133b374a9f677
zhengzexin-python <zzz.kagari@gmail.com>
2016-12-28 14:40:22 +0800

    增加文件上传

--- src/filter/Std_numFilter.java
+++ src/filter/Std_numFilter.java
@@ -2,11 +2,17 @@ package filter;
 
 import biz.StandardBiz;
 import biz.bizimpl.StandardBizImpl;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * Created by 25397 on 2016/12/24.
@@ -18,7 +24,23 @@ public class Std_numFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         StandardBiz standardBiz = new StandardBizImpl();
-        String std_num = req.getParameter("std_num");
+        DiskFileItemFactory diskFileItemFactory=new DiskFileItemFactory();
+        ServletFileUpload fileUpload=new ServletFileUpload(diskFileItemFactory);
+        fileUpload.setHeaderEncoding("utf-8");
+        String std_num=null;
+        try {
+           List<FileItem>  fileItemList= fileUpload.parseRequest((HttpServletRequest) req);
+           for (FileItem fileItem:fileItemList){
+               if (fileItem.isFormField()){
+                   if ("std_num".equals(fileItem.getFieldName())){
+                       std_num=fileItem.getString();
+                       break;
+                   }
+               }
+           }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
         if (null != standardBiz.getStandardByStd_num(std_num)) {
             PrintWriter printWriter = resp.getWriter();
             printWriter.println("<script type=\"text/javascript\" >alert(\"标准号已存在\");history.back()</script> ");
--- src/servlet/AddStandardServlet.java
+++ src/servlet/AddStandardServlet.java
@@ -57,8 +57,7 @@ public class AddStandardServlet extends HttpServlet {
                     String name=fileItem.getName();
                     File file=new File(dir,name);
                     package_path=file.getAbsolutePath();
-                    System.out.println(package_path);
-                    /*fileItem.write(file);*/
+                    fileItem.write(file);
                 }
             }
 
@@ -67,7 +66,7 @@ public class AddStandardServlet extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
-       /* Standard standard=new Standard(std_num,zhname,version,skeys,release_date,impl_date,package_path);
+        Standard standard=new Standard(std_num,zhname,version,skeys,release_date,impl_date,package_path);
         StandardBiz standardBiz=new StandardBizImpl();
         int count=standardBiz.addStandard(standard);
         if(count>0){
@@ -77,7 +76,7 @@ public class AddStandardServlet extends HttpServlet {
             printWriter.println(" <script type=\"text/javascript\">alert(\"添加失败\");history.back()</script>");
             printWriter.flush();
             printWriter.close();
-        }*/
+        }
 
     }
 
